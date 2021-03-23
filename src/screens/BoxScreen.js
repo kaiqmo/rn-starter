defd89bb7b8896e55503b6dce11709634888a62e
kaique <kaiquesilva@codeby.com.br>
2021-03-23 17:11:59 -0300

    Exercise 6

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,7 +5,9 @@ const BoxScreen = () =>{
 
     return(
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}> Box Screen</Text>
+            <Text style={styles.textStyle1}> Child 1</Text>
+            <Text style={styles.textStyle2}> Child 2 </Text>
+            <Text style={styles.textStyle3}> Child 3 </Text>
         </View>
     );
 }
@@ -13,12 +15,45 @@ const BoxScreen = () =>{
 const styles = StyleSheet.create({
     viewStyle:{
         borderWidth:1,
-        borderColor:'black'
+        borderColor:'black',
+        alignItems:'center',
+        flexDirection:'row',
+        height:100,
+        justifyContent:'center'
     },
-    textStyle:{
-        borderWidth:15,
+    textStyle1:{
+        borderWidth:1,
         borderColor:'red',
-        margin:20
+        flex:3,
+        top:0,
+        left:0,
+        backgroundColor:'red',
+        width:55,
+        height:55,
+        position:'absolute'
+    },
+    textStyle2:{
+        borderWidth:1,
+        borderColor:'red',
+        fontSize:18,
+        margin:10,
+        flex:3,
+        position:'absolute',
+        width:55,
+        height:55,
+        top:25,
+        backgroundColor:'purple'
+    },
+    textStyle3:{
+        borderWidth:1,
+        borderColor:'red',
+        flex:4,
+        top:0,
+        right:0,
+        position:'absolute',
+        width:55,
+        height:55,
+        backgroundColor:'grey'
     },
 })
 
